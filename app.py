382412926e18423ada72c0641355688421849229
Benjamin Kokou AMOUZOU <benkokouamouzou@gmail.com>
2022-03-03 08:54:59 +0000

    Message d'erreur lors de la non sélection du fichier

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
 # -*- coding: utf-8 -*-
-"""Point pour l'interface web"""
+"""Point d'entrée pour l'interface web"""
 
 __author__ = "Benjamin AMOUZOU"
 __createAt__ = "02/03/2022"
 __updateAt__ = "02/03/2022"
 
 import os
-from flask import Flask, render_template, flash, request, redirect, url_for, session
+from flask import Flask, render_template, request, redirect, url_for, session
 from werkzeug.utils import secure_filename
 from flask_session import Session
 from structs.service_data import ServiceData
@@ -28,15 +28,18 @@ def upload_file():
     if request.method == 'POST':
         # check if the post request has the file part
         if 'file' not in request.files:
-            flash('No file part')
-            return redirect(request.url)
-        file = request.files['file']
+            erreur = "Vous n'avez pas sélectionner un fichier !"
+            return render_template('index.html', erreur=erreur)
 
+        file = request.files['file']
         # If the user does not select a file, the browser submits an
         # empty file without a filename.
-        if file.filename == '':
-            flash('No selected file')
-            return redirect(request.url)
+        try:
+            if file.filename is defined == '':
+                pass
+        except:
+            erreur = "Vous n'avez pas sélectionner un fichier !"
+            return render_template('index.html', erreur=erreur)
 
         if file and allowed_file(file.filename):
             service = ServiceData.get_instance()
